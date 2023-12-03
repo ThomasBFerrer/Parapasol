250237914cbfc56977338edc9dfba88020255a0d
Thomas Ferrer <91891231+ThomasBFerrer@users.noreply.github.com>
2023-12-03 13:44:29 +0100

    IL BOUGE

--- Jean-mi.gd
+++ Jean-mi.gd
@@ -1,16 +1,27 @@
 extends CharacterBody2D
 
-export var speed = 700
-var dir = Vector2()
-const FLOOR_NORMAL = Vector2(0, -1)
+@export var speed = 400
 
+func get_input():
+	var input_direction = Input.get_vector("ui_left","ui_right","ui_up", "ui_down")
+	velocity = input_direction * speed
+	
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_left"):
-		dir.x = -speed #On va a gauche
-	elif Input.is_action_pressed("ui_right"):
-		dir.x = speed #On va a droite
-	else: 
-		dir.x = 0 #On reste static
+	get_input()
+	move_and_slide()
+	
+	if Input.is_action_pressed("ui_right"):
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = true
+	elif Input.is_action_pressed("ui_up"):
+		$AnimatedSprite2D.play("jump")
 		
-		#Déplacement du perso
-	move_and_slide(dir, FLOOR_NORMAL)
+	elif Input.is_action_pressed("frappe"):
+		$AnimatedSprite2D.play("atack2")
+	elif Input.is_action_pressed("saut"):
+		$AnimatedSprite2D.play("atack1")
+	else: 
+		$AnimatedSprite2D.play("idle")
